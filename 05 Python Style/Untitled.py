8af379d71a90007c93b892300c08ca168eba3661
dy5299 <dykgit@gmail.com>
2020-07-18 16:30:34 +0900

    python style

---
+++ 05 Python Style/Untitled.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def ssum(left, right):    #변수 간 공백
+    return (left+right, left-right)     #먼저 연산되는 연산자는 붙여서
+
+#두 칸 띄기
+val1 = 5
+val2 = 8
+print(ssum(val1, val2))
+#한 칸 띄기
+
